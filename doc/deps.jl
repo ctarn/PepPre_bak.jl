f4da71fdfd1318e37c9f177ad10775a0d9885ae5
Tarn Yeong Ching <i@ctarn.io>
2023-02-19 17:07:15 +0800

    Update deps.jl

--- doc/deps.jl
+++ doc/deps.jl
@@ -1,10 +1,4 @@
 using Pkg
 
-deps = [
-    PackageSpec(url="http://github.com/ctarn/MesCore.jl"),
-    PackageSpec(url="http://github.com/ctarn/PepIso.jl"),
-    PackageSpec(path=pwd()),
-]
-Pkg.develop(deps)
 Pkg.resolve()
 Pkg.instantiate()
