69077a468f03a248b3e883553ebfa67ce4190deb
Tarn Yeong Ching <i@ctarn.io>
2023-02-19 19:09:08 +0800

    Update build_doc.jl

--- doc/build_doc.jl
+++ doc/build_doc.jl
@@ -1,4 +1,8 @@
 import Documenter
 
 Documenter.makedocs(sitename="PepPre")
-Documenter.deploydocs(repo="github.com/ctarn/PepPre_bak.jl.git", dirname="doc")
+Documenter.deploydocs(
+    repo="github.com/ctarn/PepPre_bak.jl.git",
+    dirname="doc",
+    versions=["stable" => "v^", "v#.#", devurl => devurl],
+)
